3f0c6bfb2e525ecc475ae924f26e0ab28e9702b2
Shreya Keshive <shreyakeshive@gatech.edu>
2017-10-13 21:58:24 -0400

    Added attributes to user object

--- app/src/main/java/team/airpal/Model/User.java
+++ app/src/main/java/team/airpal/Model/User.java
@@ -5,4 +5,49 @@ package team.airpal.Model;
  */
 
 public class User {
+    private String name;
+    private String email;
+    private String password;
+    private int age;
+
+    public User(String name, String email, String password, int age) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
 }
