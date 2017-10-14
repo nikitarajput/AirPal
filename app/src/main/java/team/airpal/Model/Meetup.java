23d38d2300071e00e3652d80f1b53d8f0e4a9522
Soni Aggarwal <soniaggarwal@lawn-128-61-117-65.lawn.gatech.edu>
2017-10-14 03:46:43 -0400

    Fixed Journey

--- app/src/main/java/team/airpal/Model/Meetup.java
+++ app/src/main/java/team/airpal/Model/Meetup.java
@@ -7,19 +7,13 @@ package team.airpal.Model;
 public class Meetup {
     private String name;
     private String description;
-    private String departureAirport;
-    private String destinationAirport;
     private String location;
-    private int flightNumber;
     private User[] listOfUsers;
 
-    public Meetup (String name, String description, String departureAirport, String destinationAirport, String location, int flightNumber) {
+    public Meetup (String name, String description, String location) {
         this.name = name;
         this.description = description;
-        this.departureAirport = departureAirport;
-        this.destinationAirport = destinationAirport;
         this.location = location;
-        this.flightNumber = flightNumber;
     }
 
     public String getName() {
@@ -30,20 +24,8 @@ public class Meetup {
         return description;
     }
 
-    public String getDepartureAirport() {
-        return departureAirport;
-    }
-
-    public String getDestinationAirport() {
-        return destinationAirport;
-    }
-
     public String getLocation() {
         return location;
-    }
-
-    public int getFlightNumber() {
-        return flightNumber;
     }
 
     public User[] getListOfUsers() {
@@ -58,20 +40,8 @@ public class Meetup {
         this.description = description;
     }
 
-    public void setDepartureAirport(String departureAirport) {
-        this.departureAirport = departureAirport;
-    }
-
-    public void setDestinationAirport(String destinationAirport) {
-        this.destinationAirport = destinationAirport;
-    }
-
     public void setLocation(String location) {
         this.location = location;
-    }
-
-    public void setFlightNumber(int flightNumber) {
-        this.flightNumber = flightNumber;
     }
 
     public void setListOfUsers(User[] listOfUsers) {
