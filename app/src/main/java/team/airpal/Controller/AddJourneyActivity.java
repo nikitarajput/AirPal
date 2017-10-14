3dd003d9c58203e3c6684a337649b38f33deac74
Shreya Keshive <shreyakeshive@gatech.edu>
2017-10-14 01:09:19 -0400

    Add journey redirects to confirm

--- app/src/main/java/team/airpal/Controller/AddJourneyActivity.java
+++ app/src/main/java/team/airpal/Controller/AddJourneyActivity.java
@@ -1,7 +1,9 @@
 package team.airpal.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import team.airpal.R;
 
@@ -11,5 +13,9 @@ public class AddJourneyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_journey);
+    }
+
+    public void toConfirmActivity(View v){
+        startActivity(new Intent(AddJourneyActivity.this, ConfirmActivity.class));
     }
 }
