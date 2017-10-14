537872f8049c29ee3b467a65f5d04dcf00d05299
nikitarajput <nrajput@gatech.edu>
2017-10-14 00:20:42 -0400

    to Login Activity

--- app/src/main/java/team/airpal/Controller/WelcomeActivity.java
+++ app/src/main/java/team/airpal/Controller/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package team.airpal.Controller;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import team.airpal.R;
 
@@ -10,5 +12,9 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void toLoginActivity(View view){
+        startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
     }
 }
