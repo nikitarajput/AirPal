83228750f96a0220590ededdce3fc96536da05dd
Shreya Keshive <shreyakeshive@gatech.edu>
2017-10-14 03:47:01 -0400

    Journey created upon confirmation

--- app/src/main/java/team/airpal/Controller/AddJourneyActivity.java
+++ app/src/main/java/team/airpal/Controller/AddJourneyActivity.java
@@ -1,21 +1,31 @@
 package team.airpal.Controller;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import team.airpal.Model.Journey;
 import team.airpal.R;
 
 public class AddJourneyActivity extends AppCompatActivity {
+    TextInputEditText flightNumberTextInput;
+    static String flightNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_journey);
+        flightNumberTextInput = (TextInputEditText) findViewById(R.id.flightNumber);
     }
 
     public void toConfirmActivity(View v){
+        flightNumber = flightNumberTextInput.getText().toString();
         startActivity(new Intent(AddJourneyActivity.this, ConfirmActivity.class));
+    }
+
+    public void createJourney() {
+        Journey currentJourney = new Journey(Integer.parseInt(flightNumber));
     }
 }
