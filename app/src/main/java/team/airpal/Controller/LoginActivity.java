4d72c035a200ebb7335adee4f036b2fea8c0f0f5
Soni Aggarwal <soniaggarwal@lawn-128-61-117-65.lawn.gatech.edu>
2017-10-13 22:28:32 -0400

    Fixed Login Activity

--- app/src/main/java/team/airpal/Controller/LoginActivity.java
+++ app/src/main/java/team/airpal/Controller/LoginActivity.java
@@ -84,7 +84,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) findViewById(R.id.email);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
