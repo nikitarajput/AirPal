a72070f7c4af3c595a7e6daa1a19f3f163b9a082
Shreya Keshive <shreyakeshive@gatech.edu>
2017-10-13 22:06:37 -0400

    Added attributes to journey object

--- app/src/main/java/team/airpal/Model/Journey.java
+++ app/src/main/java/team/airpal/Model/Journey.java
@@ -1,8 +1,59 @@
 package team.airpal.Model;
 
+import java.sql.Time;
+
 /**
  * Created by Shreya on 10/13/17.
  */
 
 public class Journey {
+    private int flightNumber;
+    private String depatureAirport;
+    private String destinationAirport;
+    private Time startTime;
+    private Time endTime;
+
+    public Journey(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDepatureAirport() {
+        return depatureAirport;
+    }
+
+    public String getDestinationAirport() {
+        return destinationAirport;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public void setDepatureAirport(String depatureAirport) {
+        this.depatureAirport = depatureAirport;
+    }
+
+    public void setDestinationAirport(String destinationAirport) {
+        this.depatureAirport = destinationAirport;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
 }
